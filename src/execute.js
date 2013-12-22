c0def21c20b32e0753f07fa2c1d2570a563b20ab
takatama <h-takatama@biglobe.co.jp>
2013-12-23 01:25:24 +0900

    modify year detection
    .goog-date-picker-monthyear is not if the browser is reloaded.

--- src/execute.js
+++ src/execute.js
@@ -5,9 +5,10 @@
     var text = waypoints[0].innerText + ' → ' + waypoints[waypoints.length-3].innerText;
     var z = function(x) { return ('00'+x).slice(-2);};
     var t = function(year, month, day, hours, minutes) {var d = new Date(year, month - 1, day, hours, minutes, 0, 0); return d.getUTCFullYear()+z(d.getUTCMonth()+1)+z(d.getUTCDate())+'T'+z(d.getUTCHours())+z(d.getUTCMinutes())+'00Z';};
-    var yy = q('.goog-date-picker-monthyear')[0];
-    var year = (yy)?yy.innerText.split('年')[0] : (new Date()).getFullYear();
     var month_date = q('.date-picker>span')[0].innerText.split(/[月日]/);
+    var today = new Date();
+    var year = today.getFullYear();
+    if (month_date[0] < today.getMonth() + 1) { year += 1;}
     var b = q('.trip.selected .transit-time');
     var start_hours_minute = b[0].innerText.split(':');
     var end_hours_minute = b[1].innerText.split(':');
